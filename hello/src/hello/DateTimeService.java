04153d0ec5266abd55da4589b830b632e7028511
Alexandre-gommez <37808260+Alexandre-gommez@users.noreply.github.com>
2019-12-11 15:13:34 +0000

    Add files via upload

---
+++ hello/src/hello/DateTimeService.java
@@ -0,0 +1,55 @@
+package hello;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.io.*;
+
+public class DateTimeService
+{
+   private Calendar calendar;
+
+   //constructor creates the Calendar object, could use the constructor:
+   //   Calendar(TimeZone zone, Locale aLocale) to explicitly specify 
+   //	  the time zone and locale
+   public DateTimeService()
+   {
+	 this.calendar = Calendar.getInstance();
+   }
+
+   //method returns date/time as a formatted String object
+   public String[] getDateAndTime()
+   {
+	 Date d = this.calendar.getTime();
+	 String[] tab=new String[2];
+	 FileReader filereader = null;
+     BufferedReader bufferedreader = null;
+     try {
+         filereader = new FileReader("/sys/class/thermal/thermal_zone0/temp.txt");
+         bufferedreader = new BufferedReader(filereader);
+         String strCurrentLine;
+         while ((strCurrentLine = bufferedreader.readLine()) != null) {
+           tab[0]=strCurrentLine;
+        		   System.out.println(strCurrentLine);
+         }
+       } 
+     catch (IOException e) 
+     {
+         e.printStackTrace();
+     } 
+     finally {
+         try {
+           if (bufferedreader != null)
+             bufferedreader.close();
+           if (filereader != null)
+             filereader.close();
+         } 
+         catch (IOException e) 
+         {
+           e.printStackTrace();
+         }
+       }
+     
+     tab[1]=d.toString();
+     return tab ;	
+   }	
+}
---
+++ hello/src/hello/Test.java
@@ -0,0 +1,11 @@
+package hello;
+
+///Class for the graphic
+public class Test {
+	/// dessin graphique 
+	
+	public static void main (String[] args) {
+		System.out.println("hello World");
+	}
+}
+ 
